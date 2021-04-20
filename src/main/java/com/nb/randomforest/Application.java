f5dbe72add65271bdd7a1518b64c6de3ac2ddbf6
kaiminzhangnb <80014201+kaiminzhangnb@users.noreply.github.com>
2021-04-20 10:30:09 -0700

    disable mongo auto configuration

--- src/main/java/com/nb/randomforest/Application.java
+++ src/main/java/com/nb/randomforest/Application.java
@@ -2,6 +2,8 @@ package com.nb.randomforest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author yuxi
  * @date 2020/10/9
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
 @EnableAsync
