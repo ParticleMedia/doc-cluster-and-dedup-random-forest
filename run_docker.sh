93829e323f2fa507d8487725c9a04f21d9c2fb76
kaiminzhangnb <80014201+kaiminzhangnb@users.noreply.github.com>
2021-08-04 10:17:26 -0700

    add GC parameters

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Dspring.profiles.active=$1 -Dlogging.config=logback-spring.xml RandomForest-0.0.1.jar
+java -Xms5G -Xmx7G -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:CMSInitiatingOccupancyFraction=60 -jar -Dspring.profiles.active=$1 -Dlogging.config=logback-spring.xml RandomForest-0.0.1.jar
