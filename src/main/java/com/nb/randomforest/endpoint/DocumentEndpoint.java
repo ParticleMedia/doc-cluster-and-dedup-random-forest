c99c0462002a605d8561f94563ec4e301c944593
sysudhh <353351752@qq.com>
2023-04-28 15:26:08 +0800

    hot fix

--- src/main/java/com/nb/randomforest/endpoint/DocumentEndpoint.java
+++ src/main/java/com/nb/randomforest/endpoint/DocumentEndpoint.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
